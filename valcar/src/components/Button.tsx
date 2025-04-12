5822979db66a94bf8b6e04dca89ea2ca5af832ab
w35940aa <armaan.ahmed@student.manchester.ac.uk>
2025-04-12 17:54:33 +0100

    font adjustments

--- valcar/src/components/Button.tsx
+++ valcar/src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({label, onClick}) => {
     return(
         <button
             onClick = {onClick}
-            className="px-6 py-2 bg-blue0 text-white rounded-xl font-bold shadow-lg hover:shadow-blue-400 hover:scale-105 hover:bg-blue1 transition duration-300 ease-in-out transform cursor-pointer ring-1 ring-blue-500/40 hover:ring-2"
+            className="px-6 py-2 bg-blue0 font-Font text-white rounded-xl font-bold shadow-lg hover:shadow-blue-400 hover:scale-105 hover:bg-blue1 transition duration-300 ease-in-out transform cursor-pointer ring-1 ring-blue-500/40 hover:ring-2"
         >
             {label}
         </button>
--- valcar/src/components/NavBar.tsx
+++ valcar/src/components/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar: React.FC = () => {
     return (
         <nav className="bg-blue0 text-white px-6 py-2 shadow-md fixed top-0 w-full z-50">
             <div className="container mx-auto flex items-center justify-between">
-            <div className="text-2xl font-bold text-blue2 font-OblivionFuture">ValCar</div>
+            <div className="text-2xl font-bold text-blue2 font-Font">VALCAR</div>
                 <ul className="flex space-x-6">
-                    <li><Link to="/" className="hover:text-blue2 transition font-OblivionFuture">Home</Link></li>
-                    <li><Link to="/valuation" className="hover:text-blue2 transition font-OblivionFuture">Valuation</Link></li>
+                    <li><Link to="/" className="hover:text-blue2 transition font-Font">Home</Link></li>
+                    <li><Link to="/valuation" className="hover:text-blue2 transition font-Font">Valuation</Link></li>
                 </ul>
             </div>
         </nav>
